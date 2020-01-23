7670251a0f30cfe102e154b804fa4f286fc4bb8d
qusaihameedah <qusaimohamadhameedah@gmail.com>
2020-01-23 08:42:00 +0200

    new style

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var Op;
 var Eq = 0, New = 0, Old = 0, Number = 0, Count = 0, CountDiv = 1, FirstNum = 0, Dot = false, Coop = 1;
 var In = "";
+var EqPrint = false;
 function AC() {
-  Eq = 0, New = 0, Old = 0, Number = 0, Count = 0, CountDiv = 1, FirstNum = 0, Dot = false, Coop = 1;
-    document.getElementById('input').innerHTML = In;
+    Eq = 0, New = 0, Old = 0, Number = 0, Count = 0, CountDiv = 1, FirstNum = 0, Dot = false, Coop = 1;
+    document.getElementById('input').innerHTML = "0"; ى
     document.getElementById('output').innerHTML = "0";
+    In = "";
+    EqPrint = false;
 }
 function multipply() {
     if (Coop == 1) {
@@ -19,8 +22,11 @@ function multipply() {
     Count = 0;
     CountDiv = 0;
     Op = '*';
-    Coop = Coop + 1 ;
+    Coop = Coop + 1;
     Dot = false;
+    In += "*";
+    document.getElementById('input').innerHTML = In;
+
 }
 function div() {
     if (Coop == 1) {
@@ -35,8 +41,10 @@ function div() {
     Count = 0;
     CountDiv = 0;
     Op = '/';
-    Coop = Coop + 1 ;
+    Coop = Coop + 1;
     Dot = false;
+    In += "/";
+    document.getElementById('input').innerHTML = In;
 }
 function num1() {
     Number = 1;
@@ -49,10 +57,11 @@ function num1() {
         CountDiv = CountDiv + 1;
     }
     Old = New;
-    console.log (New);
+    console.log(New);
     document.getElementById('output').innerHTML = Old;
-    console.log (Old);
-    In+=Number ;
+    console.log(Old);
+    In += Number;
+    document.getElementById('input').innerHTML = In;
 
 }
 function num2() {
@@ -67,7 +76,8 @@ function num2() {
     }
     Old = New;
     document.getElementById('output').innerHTML = Old;
-    In+=Number ;
+    In += Number;
+    document.getElementById('input').innerHTML = In;
 
 }
 function num3() {
@@ -82,7 +92,8 @@ function num3() {
     }
     Old = New;
     document.getElementById('output').innerHTML = Old;
-    In+=Number ;
+    In += Number;
+    document.getElementById('input').innerHTML = In;
 
 }
 function add() {
@@ -97,8 +108,10 @@ function add() {
     Count = 0;
     CountDiv = 0;
     Op = '+';
-    Coop = Coop + 1 ;
+    Coop = Coop + 1;
     Dot = false;
+    In += "+";
+    document.getElementById('input').innerHTML = In;
 }
 function num4() {
     Number = 4;
@@ -112,7 +125,8 @@ function num4() {
     }
     Old = New;
     document.getElementById('output').innerHTML = Old;
-    In+=Number ;
+    In += Number;
+    document.getElementById('input').innerHTML = In;
 
 }
 function num5() {
@@ -127,7 +141,8 @@ function num5() {
     }
     Old = New;
     document.getElementById('output').innerHTML = Old;
-    In+=Number ;
+    In += Number;
+    document.getElementById('input').innerHTML = In;
 
 }
 function num6() {
@@ -142,7 +157,8 @@ function num6() {
     }
     Old = New;
     document.getElementById('output').innerHTML = Old;
-    In+=Number ;
+    In += Number;
+    document.getElementById('input').innerHTML = In;
 
 }
 function min() {
@@ -157,8 +173,11 @@ function min() {
     Count = 0;
     CountDiv = 0;
     Op = '-';
-    Coop = Coop + 1 ;
+    Coop = Coop + 1;
     Dot = false;
+    In += "-";
+    document.getElementById('input').innerHTML = In;
+
 }
 function num7() {
     Number = 7;
@@ -171,7 +190,10 @@ function num7() {
         CountDiv = CountDiv + 1;
     }
     Old = New;
+    In += Number;
     document.getElementById('output').innerHTML = Old;
+    document.getElementById('input').innerHTML = In;
+
 }
 function num8() {
     Number = 8;
@@ -185,7 +207,9 @@ function num8() {
     }
     Old = New;
     document.getElementById('output').innerHTML = Old;
-    In+=Number ;
+    In += Number;
+    document.getElementById('input').innerHTML = In;
+
 }
 function num9() {
     Number = 9;
@@ -199,7 +223,9 @@ function num9() {
     }
     Old = New;
     document.getElementById('output').innerHTML = Old;
-    In+=Number ;
+    In += Number;
+    document.getElementById('input').innerHTML = In;
+
 }
 function equal() {
     if (Op == '+') {
@@ -216,6 +242,10 @@ function equal() {
     }
     document.getElementById('output').innerHTML = Eq;
     Dot = false;
+    if (EqPrint == true) {
+        In += "=";
+        document.getElementById('input').innerHTML = In;
+    }
 
 }
 function num0() {
@@ -230,32 +260,44 @@ function num0() {
     }
     Old = New;
     document.getElementById('output').innerHTML = Old;
-    In+=Number ;
+    In += Number;
+    document.getElementById('input').innerHTML = In;
 }
 function dot() {
     document.getElementById('output').innerHTML = Old + ".";
-    Dot = true ;
+    Dot = true;
+    In += ".";
+    document.getElementById('input').innerHTML = In;
+
 }
-document.getElementById('AC').addEventListener("click",AC);
-document.getElementById('btn-multiply').addEventListener("click",multipply);
-document.getElementById('btn-div').addEventListener("click",div);
-document.getElementById('btn-num1').addEventListener("click",num1);
-document.getElementById('btn-num2').addEventListener("click",num2);
-document.getElementById('btn-num3').addEventListener("click",num3);
-document.getElementById('btn-add').addEventListener("click",add);
-document.getElementById('btn-num4').addEventListener("click",num4);
-document.getElementById('btn-num5').addEventListener("click",num5);
-document.getElementById('btn-num6').addEventListener("click",num6);
-document.getElementById('btn-min').addEventListener("click",min);
-document.getElementById('btn-num7').addEventListener("click",num7);
-document.getElementById('btn-num8').addEventListener("click",num8);
-document.getElementById('btn-num9').addEventListener("click",num9);
-document.getElementById('btn-equ').addEventListener("click",equal);
-document.getElementById('btn-num0').addEventListener("click",num0);
-document.getElementById('btn-dot').addEventListener("click",dot);
+function Prtint() {
+    EqPrint = true
 
+}
+document.getElementById('AC').addEventListener("click", AC);
+document.getElementById('btn-multiply').addEventListener("click", multipply);
+document.getElementById('btn-div').addEventListener("click", div);
+document.getElementById('btn-num1').addEventListener("click", num1);
+document.getElementById('btn-num2').addEventListener("click", num2);
+document.getElementById('btn-num3').addEventListener("click", num3);
+document.getElementById('btn-add').addEventListener("click", add);
+document.getElementById('btn-num4').addEventListener("click", num4);
+document.getElementById('btn-num5').addEventListener("click", num5);
+document.getElementById('btn-num6').addEventListener("click", num6);
+document.getElementById('btn-min').addEventListener("click", min);
+document.getElementById('btn-num7').addEventListener("click", num7);
+document.getElementById('btn-num8').addEventListener("click", num8);
+document.getElementById('btn-num9').addEventListener("click", num9);
+document.getElementById('btn-equ').addEventListener("click", Prtint);
+document.getElementById('btn-equ').addEventListener("click", equal);
+document.getElementById('btn-num0').addEventListener("click", num0);
+document.getElementById('btn-dot').addEventListener("click", dot);
 
-
-
-
-
+let buttons = document.getElementsByTagName('button');
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function (e) {
+        setTimeout(function () {
+            e.target.blur();
+        }, 100);
+    });
+}
